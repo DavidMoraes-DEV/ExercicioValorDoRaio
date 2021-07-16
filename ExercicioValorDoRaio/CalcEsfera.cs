e3a235df5e31baaec6db760bdc6aa5f52fa21041
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2021-07-16 14:35:04 -0300

    Exercicio valor do Raio de uma Esfera

---
+++ ExercicioValorDoRaio/CalcEsfera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioValorDoRaio
+{
+    class CalcEsfera
+    {
+        public double Raio;
+        public double PI = 3.14;
+
+        public double Circunferencia()
+        {
+            return 2 * PI * Raio;    
+        }
+
+        public double Volume ()
+        {
+            return 4 * PI * Math.Pow(Raio, 3) / 3;
+        }
+    }
+}
---
+++ ExercicioValorDoRaio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioValorDoRaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcEsfera esfera = new CalcEsfera();
+
+            Console.Write("Entre o valor do raio: ");
+            esfera.Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("\nCircunferência: " + esfera.Circunferencia().ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("\nVolume: " + esfera.Volume().ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("\nValor de PI: " + esfera.PI.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+/*
+ Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor de uma
+circuferência e do volume de uma esfera para um raio daquele valor. Informar também o valor de PI
+com duas casas decimais.
+
+Exemplo:
+
+Digite o valor do raio: 3.0
+Cirfunferência: 18.84
+Volume: 113.04
+Valor de PI: 3.14
+ */
